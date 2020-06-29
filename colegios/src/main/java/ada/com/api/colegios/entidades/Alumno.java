be590705aaf068b5789a78cc29cd7dc0f279b0af
ydauracontreras <ydauracontreras@gmail.com>
2020-06-29 11:34:27 -0300

    Creacion de la clase curso

--- colegios/src/main/java/ada/com/api/colegios/entidades/Alumno.java
+++ colegios/src/main/java/ada/com/api/colegios/entidades/Alumno.java
@@ -1,61 +1,67 @@
 package ada.com.api.colegios.entidades;
 
 public class Alumno {
-    private String nombre;
-    private String apellido;
-    private Integer codigoAlumno;
+  private String nombre;
+  private String apellido;
+  private Integer codigoAlumno;
 
-    this.nombre=nombre;this.apellido=apellido;this.codigoAlumno=codigoAlumno;
-    }
+  /**
+   * @param nombre
+   * @param apellido
+   * @param codigoAlumno
+   */
+  public Alumno(String nombre, String apellido, Integer codigoAlumno) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.codigoAlumno = codigoAlumno;
+  }
 
-    
+  public boolean equals(Object alumno) {
+    Alumno a = (Alumno) alumno;
+    return this.getCodigoAlumno().equals(a.getCodigoAlumno());
 
+  }
 
-    public boolean equals(Object o){
-     
+  /**
+   * @return the nombre
+   */
+  public String getNombre() {
+    return nombre;
+  }
 
-          
+  /**
+   * @param nombre the nombre to set
+   */
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
 
-            
-        
-    /
+  /**
+   * @return the apellido
+   */
+  public String getApellido() {
+    return apellido;
+  }
 
-     * 
-     * @ret
-     * 
-     * lic String getN
-     * 
-     * 
-     * 
-     * 
-     *       */
-     ublic void setNombre(String nombre) {
-     th
-    is.
+  /**
+   * @param apellido the apellido to set
+   */
+  public void setApellido(String apellido) {
+    this.apellido = apellido;
+  }
 
-    	 */
-    
-	public String getApellido() {
-		return 
-    pellido;
+  /**
+   * @return the codigoAlumno
+   */
+  public Integer getCodigoAlumno() {
+    return codigoAlumno;
+  }
 
-	
-	 */
-	public Integer getCodigoAlumno() {
-		return codigoAlumno;
-	}
-	/**
-	 * @param codigoAlumno the codigoAlumno to set
-	 */
-	public void setCodigoAlumno(Integer codigoAlumno) {
-		this.codigoAlumno = codigoAlumno;
-	}
+  /**
+   * @param codigoAlumno the codigoAlumno to set
+   */
+  public void setCodigoAlumno(Integer codigoAlumno) {
+    this.codigoAlumno = codigoAlumno;
+  }
 
-
-    
-
-
-    
 }
-
-
---
+++ colegios/src/main/java/ada/com/api/colegios/entidades/Curso.java
@@ -0,0 +1,40 @@
+package ada.com.api.colegios.entidades;
+
+public class Curso {
+    public String nombre;
+    public String codigoCurso;
+
+    public boolean equals(Object c) {
+        Curso curso = (Curso) c;
+        return this.getCodigoCurso().equals(curso.getCodigoCurso());
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the codigoCurso
+     */
+    public String getCodigoCurso() {
+        return codigoCurso;
+    }
+
+    /**
+     * @param codigoCurso the codigoCurso to set
+     */
+    public void setCodigoCurso(String codigoCurso) {
+        this.codigoCurso = codigoCurso;
+    }
+
+}
