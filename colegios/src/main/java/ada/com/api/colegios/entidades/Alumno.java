2b5b2f5569e80dccf905a9595e3a3d01c12acf2f
ydauracontreras <ydauracontreras@gmail.com>
2020-06-29 11:15:19 -0300

    Api para el registro de Alumnos y profesores

---
+++ colegios/src/main/java/ada/com/api/colegios/entidades/Alumno.java
@@ -0,0 +1,61 @@
+package ada.com.api.colegios.entidades;
+
+public class Alumno {
+    private String nombre;
+    private String apellido;
+    private Integer codigoAlumno;
+
+    this.nombre=nombre;this.apellido=apellido;this.codigoAlumno=codigoAlumno;
+    }
+
+    
+
+
+    public boolean equals(Object o){
+     
+
+          
+
+            
+        
+    /
+
+     * 
+     * @ret
+     * 
+     * lic String getN
+     * 
+     * 
+     * 
+     * 
+     *       */
+     ublic void setNombre(String nombre) {
+     th
+    is.
+
+    	 */
+    
+	public String getApellido() {
+		return 
+    pellido;
+
+	
+	 */
+	public Integer getCodigoAlumno() {
+		return codigoAlumno;
+	}
+	/**
+	 * @param codigoAlumno the codigoAlumno to set
+	 */
+	public void setCodigoAlumno(Integer codigoAlumno) {
+		this.codigoAlumno = codigoAlumno;
+	}
+
+
+    
+
+
+    
+}
+
+
---
+++ colegios/src/test/java/ada/com/api/colegios/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ada.com.api.colegios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
